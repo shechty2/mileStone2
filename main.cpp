809cf58576f3484a0a3fecf72f653583dc42f5e4
shechty2 <59018548+shechty2@users.noreply.github.com>
2020-02-10 22:34:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,15 @@
 #include <iostream>
 using namespace Boot;
 using namespace std;
-    int main() {
-      Main* m = new Main();
-      m->main(5600);
-      return 0;
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        auto *m = new Main();
+        m->main(5600);
+    } else {
+        auto *m = new Main();
+        string ps = argv[1];
+        int p = stoi(ps);
+        m->main(p);
     }
-
-
+    return 0;
+}
